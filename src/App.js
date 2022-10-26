d7955b13b0118c3c87c4af0e9318be35fb792c7f
Sandesha98 <sandesha.bcss19@iba-suk.edu.pk>
2022-10-26 10:31:12 +0500

    project added

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import 'antd/dist/antd.css';
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'antd';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button type='primary' size='large'>hello</Button>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
